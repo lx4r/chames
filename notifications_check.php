664e7f2f9de8b03f613d715edc2a746514271b24
lx4r <k@l3r.de>
2016-03-26 19:02:30 +0100

    Fixed email problem when no games were in the watch list

--- notifications_check.php
+++ notifications_check.php
@@ -4,33 +4,36 @@ require_once('get_game_data.php');
 require_once('data_management.php');
 
 $data = LoadData($configDataFile);
-$changed = false;
 
-foreach ($data as $key => $game){
-    if ($game['active']){
-        $auctionData = GetAuctionData($game['gameID']);
-        if ($auctionData['price'] <= $game['notificationLimit']){
-            $emailText = $configEmailText;
-            $emailText = str_replace('{gameName}', $game['gameName'], $emailText);
-            $emailText = str_replace('{gamePrice}', $auctionData['price'], $emailText);
-            $emailText = str_replace('{sellerCountry}', $auctionData['country'], $emailText);
-            $emailText = str_replace('{sellerRating}', $auctionData['rating'], $emailText);
-            $emailText = str_replace('{sellerSells}', $auctionData['sells'], $emailText);
-            $emailSubject = str_replace('{gameName}', $game['gameName'], $configEmailSubject);
-            $emailHeader =
-                'From: ' . $configSenderEmail . "\r\n" .
-                'X-Mailer: PHP/' . phpversion() . "\r\n" .
-                "Content-type: text/html; charset=UTF-8";
+if ($data == null){
+    $changed = false;
 
-            mail($configUserEmail, $emailSubject, $emailText, $emailHeader);
-            $data[$key]['active'] = false;
-            if(!$changed){
-                $changed = true;
+    foreach ($data as $key => $game){
+        if ($game['active']){
+            $auctionData = GetAuctionData($game['gameID']);
+            if ($auctionData['price'] <= $game['notificationLimit']){
+                $emailText = $configEmailText;
+                $emailText = str_replace('{gameName}', $game['gameName'], $emailText);
+                $emailText = str_replace('{gamePrice}', $auctionData['price'], $emailText);
+                $emailText = str_replace('{sellerCountry}', $auctionData['country'], $emailText);
+                $emailText = str_replace('{sellerRating}', $auctionData['rating'], $emailText);
+                $emailText = str_replace('{sellerSells}', $auctionData['sells'], $emailText);
+                $emailSubject = str_replace('{gameName}', $game['gameName'], $configEmailSubject);
+                $emailHeader =
+                    'From: ' . $configSenderEmail . "\r\n" .
+                    'X-Mailer: PHP/' . phpversion() . "\r\n" .
+                    "Content-type: text/html; charset=UTF-8";
+
+                mail($configUserEmail, $emailSubject, $emailText, $emailHeader);
+                $data[$key]['active'] = false;
+                if(!$changed){
+                    $changed = true;
+                }
             }
         }
     }
-}
-if ($changed){
-    SaveData($configDataFile, $data);
+    if ($changed){
+        SaveData($configDataFile, $data);
+    }
 }
 ?>
