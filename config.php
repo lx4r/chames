c453afad739c2938cf4fcef6023faa97188891b1
lx4r <k@l3r.de>
2016-03-26 18:53:26 +0100

    Fixed notification email problem

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $configEmailText =
     The seller is from {sellerCountry} and has a rating of {sellerRating}% (based on {sellerSells} sells).<br>
     <br>
     The alert for this game is now disabled. Please go to the web interface of this tool to reactivate it.<br>
+    <br>
     Your price watchdog";
 $configEmailSubject = "{gameName} is now available for your desired price";
 $configDataFile = __DIR__ . '/data.json';
--- notifications_check.php
+++ notifications_check.php
@@ -4,25 +4,33 @@ require_once('get_game_data.php');
 require_once('data_management.php');
 
 $data = LoadData($configDataFile);
+$changed = false;
 
 foreach ($data as $key => $game){
-    $auctionData = GetAuctionData($game['gameID']);
-    if ($auctionData['price'] <= $game['notificationLimit']){
-        $emailText = $configEmailText;
-        $emailText = str_replace('{gameName}', $game['gameName'], $emailText);
-        $emailText = str_replace('{gamePrice}', $auctionData['price'], $emailText);
-        $emailText = str_replace('{sellerCountry}', $auctionData['country'], $emailText);
-        $emailText = str_replace('{sellerRating}', $auctionData['rating'], $emailText);
-        $emailText = str_replace('{sellerSells}', $auctionData['sells'], $emailText);
-        $emailSubject = str_replace('{gameName}', $game['gameName'], $configEmailSubject);
-        $emailHeader =
-            'From: ' . $configSenderEmail . "\r\n" .
-            'X-Mailer: PHP/' . phpversion() . "\r\n" .
-            "Content-type: text/html; charset=UTF-8";
+    if ($game['active']){
+        $auctionData = GetAuctionData($game['gameID']);
+        if ($auctionData['price'] <= $game['notificationLimit']){
+            $emailText = $configEmailText;
+            $emailText = str_replace('{gameName}', $game['gameName'], $emailText);
+            $emailText = str_replace('{gamePrice}', $auctionData['price'], $emailText);
+            $emailText = str_replace('{sellerCountry}', $auctionData['country'], $emailText);
+            $emailText = str_replace('{sellerRating}', $auctionData['rating'], $emailText);
+            $emailText = str_replace('{sellerSells}', $auctionData['sells'], $emailText);
+            $emailSubject = str_replace('{gameName}', $game['gameName'], $configEmailSubject);
+            $emailHeader =
+                'From: ' . $configSenderEmail . "\r\n" .
+                'X-Mailer: PHP/' . phpversion() . "\r\n" .
+                "Content-type: text/html; charset=UTF-8";
 
-        mail($configUserEmail, $emailSubject, $emailText, $emailHeader);
-        $data[$key]['active'] = false;
-        SaveData($configDataFile, $data);
+            mail($configUserEmail, $emailSubject, $emailText, $emailHeader);
+            $data[$key]['active'] = false;
+            if(!$changed){
+                $changed = true;
+            }
+        }
     }
+}
+if ($changed){
+    SaveData($configDataFile, $data);
 }
 ?>
