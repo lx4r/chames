f84d90cf582c6a567e84fe71347b34c1d44cd9d0
lx4r <hallo@l3r.de>
2016-05-16 00:38:16 +0200

    Small improvements in notifications_check.php

--- notifications_check.php
+++ notifications_check.php
@@ -3,7 +3,7 @@ require_once('config.php');
 require_once('get_game_data.php');
 require_once('data_management.php');
 
-$data = LoadData($configDataFile);
+$data = LoadData($config['dataFile']);
 
 if ($data == null){
     $changed = false;
@@ -13,10 +13,10 @@ if ($data == null){
             $auctionData = GetAuctionData($game['gameID']);
             if ($auctionData['price'] <= $game['notificationLimit']){
 
+                /* Replace the placeholders in the email strings with the actual values */
                 $placeholders = ['{gameName}', '{gamePrice}', '{gameURL}', '{sellerCountry}', '{sellerRating}', '{sellerSells}'];
                 $values = [$game['gameName'], $auctionData['price'], $game['gameURL'], $auctionData['country'], $auctionData['rating'], $auctionData['sells']];
                 $emailText = str_replace($placeholders, $values, $config['emailText']);
-
                 $emailSubject = str_replace('{gameName}', $game['gameName'], $config['emailSubject']);
 
                 $emailHeader =
@@ -24,8 +24,8 @@ if ($data == null){
                     'X-Mailer: PHP/' . phpversion() . "\r\n" .
                     "Content-type: text/html; charset=UTF-8";
 
+                /* Send the email and disable the alert for this game*/
                 mail($config['userEmail'], $emailSubject, $emailText, $emailHeader);
-
                 $data[$key]['active'] = false;
 
                 if(!$changed){
@@ -34,8 +34,9 @@ if ($data == null){
             }
         }
     }
+    /* If an email was sent save the new status of the game's alert (now disbaled) to the data file */
     if ($changed){
-        SaveData($configDataFile, $data);
+        SaveData($config['dataFile'], $data);
     }
 }
 ?>
